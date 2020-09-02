d940e0b4397196667e9846cda51997bfe1144912
meenakshi kumar <meenakshi.mk33@gmail.com>
2020-09-02 01:31:09 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+// import logo from './logo.svg';
+
+class Hello extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>github card project </p>
+      </div>
+    );
+  }
+}
+export default Hello;
